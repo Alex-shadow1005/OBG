cd205a48f4aba734b0fd791094470cec0baf77da
Alex-shadow1005 <kaareudlejning@gmail.com>
2022-03-14 12:29:04 +0100

    Initial commit

---
+++ src/main/java/com/example/obg/ObgApplication.java
@@ -0,0 +1,13 @@
+package com.example.obg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/obg/ObgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
